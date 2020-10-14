a3a4a84176863ce8817db3aef54a7aef8b2a2f57
tsedbrpp <tod.sedbrook@gmail.com>
2020-10-14 10:52:34 -0400

    initial

---
+++ precache-manifest.9463294b52ac17df1e4fd576399d01ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8e47bbb3d508880e291a6b47885052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acdca25c43e588d24f92",
+    "url": "/static/css/main.af76d03a.chunk.css"
+  },
+  {
+    "revision": "c34f1fa574e29ede7a64",
+    "url": "/static/js/2.57d04513.chunk.js"
+  },
+  {
+    "revision": "5599c98b7af73b31fed6a721c4f935e7",
+    "url": "/static/js/2.57d04513.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acdca25c43e588d24f92",
+    "url": "/static/js/main.94bf8de1.chunk.js"
+  },
+  {
+    "revision": "64a770054e6c15c03eb3",
+    "url": "/static/js/runtime-main.5c0568b5.js"
+  }
+]);
